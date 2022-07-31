00eedf17dc1946ad4827baa1833360774c94277f
Maria Korneva <maria.korneva8731@gmail.com>
2022-07-31 18:18:11 +0400

    https://zuev-family.atlassian.net/browse/PROJ-5

--- src/Translit.jsx
+++ src/Translit.jsx
@@ -1,8 +1,13 @@
 import React, { useState } from "react";
+import CyrillicToTranslit from 'cyrillic-to-translit-js';
+
+
 
 export const Translit = () => {
   const [value, setValue] = useState('');
-
+  const cyrillicToTranslit = new CyrillicToTranslit();
+  cyrillicToTranslit.transform('Какая-то строка', '_').toLowerCase();
+  
   return <div>
     <textarea value={value} onChange={
       event=>{
